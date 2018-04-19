010a95e5de1360db5c37a05767f1abd0a556abc7
Clara Park <cycp@berkeley.edu>
2018-04-19 15:20:58 -0700

    models, migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419214404) do
+ActiveRecord::Schema.define(version: 20180419220952) do
 
   create_table "communities", force: :cascade do |t|
     t.string "Garden"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20180419214404) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
