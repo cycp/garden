d3d327d54eb04748a28f3707503e6d2565c60538
akashgokul <akashgokul@berkeley.edu>
2018-04-19 18:13:32 -0700

    Added devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419220952) do
+ActiveRecord::Schema.define(version: 20180420010919) do
 
   create_table "communities", force: :cascade do |t|
     t.string "Garden"
@@ -23,12 +23,16 @@ ActiveRecord::Schema.define(version: 20180419220952) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_community_gardens_on_user_id"
   end
 
   create_table "gardens", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
   create_table "plants", force: :cascade do |t|
@@ -46,6 +50,18 @@ ActiveRecord::Schema.define(version: 20180419220952) do
     t.integer "ax"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
