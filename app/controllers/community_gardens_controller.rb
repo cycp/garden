0cb301233c115c0c6dcbd0b3b4c7c9e7be99435a
Clara Park <cycp@berkeley.edu>
2018-04-25 16:57:39 -0700

    added garden rendering to user profile, need to deal with multiple users for a single community garden

---
+++ app/controllers/community_gardens_controller.rb
@@ -0,0 +1,2 @@
+class CommunityGardensController < ApplicationController
+end
---
+++ test/controllers/community_gardens_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CommunityGardensControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
