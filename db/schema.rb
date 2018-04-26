96df357934c418b9567fd44ee8babbc81695c652
Michelle Zhang <mzhang32@gmail.com>
2018-04-26 16:17:35 -0700

    fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426220306) do
+ActiveRecord::Schema.define(version: 20180426224748) do
 
   create_table "community_gardens", force: :cascade do |t|
     t.string "name"
