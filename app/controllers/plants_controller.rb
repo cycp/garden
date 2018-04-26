368ceb66faa8746f700038930cfe2e653d378959
Clara Park <cycp@berkeley.edu>
2018-04-26 16:32:42 -0700

    fix plant.new

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,15 +2,18 @@ class PlantsController < ApplicationController
 
 before_action :authenticate_user!
   def new
-    permitted = params.require(:id, :garden).permit(:plantID,:garden)
-    @plant = Plant.new(id:permitted[:id],garden:permitted[:garden])
-    if @plant.valid?
-      @plant.save
-    end
+    # permitted = params.require(:id, :garden).permit(:plantID,:garden)
+    # @plant = Plant.new(id:permitted[:id],garden:permitted[:garden])
+    # if @plant.valid?
+    #   @plant.save
+    # end
+    @plant = Plant.new
+  end
 
     def index
       @plants = Plant.all
     end
+
     def show
       #Change this!
       @plant = Plant.all
@@ -18,11 +21,12 @@ before_action :authenticate_user!
 
     def create
     	plant = Plant.create(plant_params)
+    	# plant.
     end
+
     def plant_params 
 		params.require(:plant).permit(:ndex)
 	end
 
-
-
+end
 end
